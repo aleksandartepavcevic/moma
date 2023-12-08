2416c7bb8e2098e74fbe647f95a06f2dde8e454f
Aleksandar Tepavcevic <atepavcevic29@gmail.com>
2023-12-08 19:38:57 +0100

    force dynamic on home route

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import BundleList from "@/components/table";
 import { db } from "./db";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const bundles = await db.bundle.findMany();
 
